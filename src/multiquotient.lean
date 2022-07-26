6afb188636457cad15a096c13fce42421eb1e1f7
Giles Shaw <gilesgshaw@gmail.com>
2022-07-27 00:42:27 +0100

    feat - computation principle for m-quotients

--- src/multiquotient.lean
+++ src/multiquotient.lean
@@ -3,7 +3,9 @@ open vector
 
 /-
 We recreate the familiar theory about lifting maps through quotients,
-but for multivariate functions (i.e. functions `(vector α n) → β`).
+but for multivariate functions (i.e. functions `(vector α n) → β`). Namely,
+we introduce counterparts to the `mk`, `ind`, `lift` and `sound` declerations
+in `quot` and `quotient`, as well as the relevant 'computation principles'.
 -/
 
 namespace multi
@@ -88,6 +90,13 @@ namespace multi
     f := quotient_off_head map,
     resp := quotient_is_respectful map}
 
+    -- useful for rewriting
+    protected lemma compute (f : (vector α n.succ) → β) : ∀ resp, ∀ {{head : α}} {{tail : vector α n}},
+    (quotient_off_head {f := f, resp := resp}) tail (quot.mk r head) = f (head ::ᵥ tail) := λ _ _ _, rfl
+
+    protected lemma compute_map (map : n+1-var_map_to β) : ∀ {{head : α}} {{tail : vector α n}},
+    (quotient_off_head map) tail (quot.mk r head) = map.f (head ::ᵥ tail) := λ _ _, rfl
+
   end
 
   universe u -- induction is much happier dealing with one universe at a time!
@@ -102,8 +111,6 @@ end multi
 
 namespace mquot
   open multi
-
-  -- counterparts to `quot.mk`, `quot.ind`, `quot.lift` and `quot.sound`.
 
   def mk {α : Type*} (r : rel α) {n : ℕ} :
   vector α n → vector (quot r) n :=
@@ -151,12 +158,54 @@ namespace mquot
     exact quot.sound (h i),
   end
 
+
+  /- Analog of the formula `quot.lift f _ (quot.mk a) = f a`, which is baked into lean
+  -- as a reduction rule, more primitive than an axiom even. Unfortunately the higher
+  -- dimensional version does require a proof, and an annoying one at that.         -/
+
+  @[simp] theorem comp {α : Type u} {r : rel α} {ref : reflexive r}
+  {n : ℕ} {β : Type u} (f : (vector α n) → β) {{v : vector α n}} :
+  ∀ h_resp, lift f ref h_resp (mk r v) = f v :=
+  begin
+
+    revert β,
+    induction n with n h_ind,
+
+    /- base case -/
+
+    intros β f h_resp,
+    simp_rw [lift, lift_internal],
+    apply congr_arg,
+    exact subsingleton.elim _ _,
+
+    /- induction -/
+
+    revert v,                                             -- break head off of vector
+    rw holds_for_all_heads_and_tails,
+    intros head tail,
+
+    intros β func h_resp,                                 -- take parameters
+    let map : multivar_map := {f := func, resp := h_resp},
+    let reduced_map := quotient_off_head ref map,
+
+    specialize @h_ind tail (quot r → β),                  -- find previous case
+    specialize @h_ind (quotient_off_head ref map),
+    specialize @h_ind (quotient_is_respectful ref map),
+
+    simp_rw [lift, lift_internal, mk] at *,
+    simp_rw [head_map, tail_map, head_cons, tail_cons],
+    simp_rw [← multi.compute ref func h_resp],            -- match them up
+    simp_rw [← map, ← reduced_map] at *,
+
+    apply congr_fun,
+    exact h_ind,                                           -- finish off
+
+  end
+
 end mquot
 
 namespace mquotient
   open multi
-
-  -- counterparts to `quotient.mk`, `quotient.ind`, `quotient.lift` and `quotient.sound`.
 
   def mk {α : Type*} [s : setoid α] {n : ℕ} :
   vector α n → vector (quotient s) n :=
@@ -173,5 +222,9 @@ namespace mquotient
 
   theorem sound {α : Type*} [s : setoid α] {n : ℕ} {a b : vector α n} :
   (∀ i, setoid.r (a.nth i) (b.nth i)) → mk a = mk b := mquot.sound
+
+  @[simp] theorem comp {α : Type u} [s : setoid α]
+  {n : ℕ} {β : Type u} (f : (vector α n) → β) {{v : vector α n}} :
+  ∀ h_resp, lift f h_resp (mk v) = f v := λ h_resp, mquot.comp f h_resp
 
 end mquotient
