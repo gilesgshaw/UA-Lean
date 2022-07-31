ad4419c53715796de1845ecf72f8a961ff3e5b4e
Giles Shaw <gilesgshaw@gmail.com>
2022-07-31 17:42:38 +0100

    loose - lemma: vector.map composition

--- src/loose.lean
+++ src/loose.lean
@@ -1,4 +1,5 @@
 import logic.nonempty
+import data.vector.basic
 
 /-
 Lemmas that are either just used when a proof is in development,
@@ -17,3 +18,13 @@ noncomputable def section_of_quot {α : Type*} (r : α → α → Prop) (q : quo
 theorem section_of_quot_section {α : Type*} {r : α → α → Prop} :
 quot.mk r ∘ section_of_quot r = id :=
 funext (λ q, (classical.choice (nonempty_subtype.mpr (quot.exists_rep q))).property)
+
+lemma vector.map_comp_2 {α : Type*} {β : Type*} {γ : Type*} {n : ℕ} {f : α → β} {g : β → γ} {v : vector α n} :
+vector.map (g ∘ f) v = vector.map g (vector.map f v) :=
+begin
+  apply vector.ext,
+  simp,
+end
+
+lemma vector.map_comp {α : Type*} {β : Type*} {γ : Type*} {n : ℕ} {f : α → β} {g : β → γ} :
+@vector.map _ _ n (g ∘ f) = (vector.map g) ∘ (vector.map f) := funext (λ v, vector.map_comp_2)
