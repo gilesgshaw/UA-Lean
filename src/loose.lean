4c7e48facf4571fefcb024a6996b118683680516
Giles Shaw <gilesgshaw@gmail.com>
2022-07-31 17:58:03 +0100

    loose - lemma: partial section
    choose partial section of arbitrary function

--- src/loose.lean
+++ src/loose.lean
@@ -12,6 +12,15 @@ lemma ap_fun {α : Type*} {β : Type*} {f : α → β} {x : α} :
 lemma specialize_by_fun {α : Type*} {β : Type*} (f : α → β) {p : β → Prop} :
 (∀ y, p y) → (∀ x, p (f x)) := λ h x, h (f x)
 
+noncomputable def partial_section {α : Type*} {β : Type*} (f : α → β) : (set.range f) → α := λ y,
+@classical.choice {x // f x = y} (nonempty_subtype.mpr (by {cases y.property with x _, use x, assumption}))
+
+theorem partial_section_prop {α : Type*} {β : Type*} {f : α → β} {y : β} (h : y ∈ set.range f) :
+f (partial_section f ⟨y, h⟩) = y := (@classical.choice {x // f x = y} _).prop
+
+theorem partial_section_prop_2 {α : Type*} {β : Type*} {f : α → β} :
+f ∘ partial_section f = subtype.val := funext (λ y, partial_section_prop y.prop)
+
 noncomputable def section_of_quot {α : Type*} (r : α → α → Prop) (q : quot r) : α :=
 @classical.choice {a // quot.mk r a = q} (nonempty_subtype.mpr (quot.exists_rep q))
 
